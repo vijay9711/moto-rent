c609c139afa848dbf54fbc553e782767d7217144
vijay9711 <vijaysm97@gmail.com>
2024-10-11 13:30:03 -0500

    tailwind intergrated

--- src/App.js
+++ src/App.js
@@ -3,21 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex h-screen w-screen align-middle items-center">
+      <h1 className="text-3xl font-bold underline m-auto animate-bounce ">
+        Welcome to moto-rent!
+      </h1>
     </div>
   );
 }
